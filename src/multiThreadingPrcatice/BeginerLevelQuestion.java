1eb7458c80641eb5a543f92c3ff04c71ec1e911d
Monalisa Das <monalisadas575@gmail.com>
2026-05-28 07:56:42 +0530

    4th commit

---
+++ src/multiThreadingPrcatice/BeginerLevelQuestion.java
@@ -0,0 +1,11 @@
+package multiThreadingPrcatice;
+
+import java.lang.reflect.Array;
+
+public class BeginerLevelQuestion {
+    public static void main(String[] args) {
+
+        //1. print even and odd number using 2 thread
+
+    }
+}
